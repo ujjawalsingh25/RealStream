27952e9cab6c7a0ec33b890ffc4d3c28eabedbde
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-09-02 23:05:10 +0530

    Setup MySql database with Prisma schema

--- commands&Lib/Commands.js
+++ commands&Lib/Commands.js
@@ -1,13 +1,36 @@
 
-// Next JS
-    // npx create-next-app@latest file-name         -->> To create project
+// ____________   NEXT JS  ______________________
+//                                               |    
+// Next JS      -->> To create project           |
+    // npx create-next-app@latest file-name      |    
+// ______________________________________________|
 
 
-// shadcn
-    // npx shadcn@latest init        -->> Run the CLI
-    // npm install next-themes       -->> themes for Dark Mode
+// ______________    shadcn   __________________________________________
+//                                                                      |
+    // npx shadcn@latest init        -->> Run the CLI                   |
+    // npm install next-themes       -->> themes for Dark Mode          |
+// _____________________________________________________________________|
 
 
-// Clerk NextJS -->  Dashboard for Authentication Dashboard
-    //  npm install @clerk/nextjs
-    // npm i @clerk/themes    -->> Clerk themes for Dark Mode (Make Appreance -> Dark in ClerkContextProvider)
+// _______________________________   Clerk NextJS   ______________________________________________________________
+//                                                                                                                |  
+// Clerk NextJS  -->>  For Authentication Dashboard                                                               |
+    //  npm install @clerk/nextjs                                                                                 |
+    // npm i @clerk/themes    -->> Clerk themes for Dark Mode (Make Appreance -> Dark in ClerkContextProvider)    |
+// _______________________________________________________________________________________________________________|
+
+
+// _______________________________   PRISMA   ______________________________________________________________
+//                                                                                                          |
+//       PRISMA   -->>  For Database Connection                                                             |
+// npm i -D prisma                                                                                          |
+// npm i @prisma/client                                                                                     |
+// npx prisma init                                                                                          |
+// npx prisma db pull       // X //    -->> If DB and tables are created in MySQL serverWorkbench.          |
+//                                  -->> It turn your database schema into a Prisma schema                  |
+// npx prisma db push                                                                                       |
+// npx prisma generate                                                                                      |
+// npx prisma studio          -->> *** To host DB to the WebServer                                          |
+// _________________________________________________________________________________________________________|
+
