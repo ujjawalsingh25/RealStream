4c12e3fd518492bc649c351c4cbf172f3d5efaa0
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-09-08 20:27:15 +0530

    Added Header Component

---
+++ components/stream-player/actions.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { toast } from 'sonner';
+import { Heart } from 'lucide-react';
+import { useTransition } from 'react';
+import { useAuth } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
+import { onFollow, onUnfollow } from '@/actions/follow';
+
+interface ActionProps {
+    hostIdentity: string;
+    isFollowing: boolean;
+    isHost: boolean;
+};
+
+export const Actions = ({hostIdentity, isFollowing, isHost}: ActionProps) => {
+    const [isPending, startTransition ] = useTransition();
+    const router = useRouter();
+    const { userId } = useAuth();
+
+    const handleFollow = () => {
+        startTransition(() => {
+            onFollow(hostIdentity)
+                .then((data) => toast.success(`Started Following ${data.following.username}`))
+                .catch(() => toast.error("Something went wrong"));
+        });
+    }
+
+    const handleUnfollow = () => {
+        startTransition(() => {
+            onUnfollow(hostIdentity)
+                .then((data) => toast.success(`Unfollowed ${data.following.username}`))
+                .catch(() => toast.error("Something went wrong"));
+        });
+    }
+
+    const toggleFollow = () => {
+        if(!userId) {
+            return router.push("/sign-in");
+        }
+        if(isHost)  return;
+
+        if(isFollowing) {
+            handleUnfollow();
+        } else {
+            handleFollow();
+        }
+    }
+    
+
+    return(
+        <Button
+            disabled={isPending}
+            onClick={toggleFollow || isHost}
+            variant="primary"
+            size="sm"
+            className="w-full lg:w-auto"
+        >
+            <Heart className={cn(
+                "h-4 w-4 mr-2",
+                isFollowing ? "fill-white" : "fill-none"
+            )}/>
+            {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+    );
+};
+
+
+export const ActionSkeleton = () => {
+    return (
+        <Skeleton className='h-10 w-full lg:w-24' />
+    )
+}
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -7,9 +7,10 @@ import { cn  } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 
-import { Video, VideoSkeleton } from "./video";
-import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
+import { Chat, ChatSkeleton } from "./chat";
+import { Video, VideoSkeleton } from "./video";
+import { Header, HeaderSkeleton } from "./header";
 
 interface StreamPLayerProps {
     user: User & { stream: Stream | null };
@@ -47,6 +48,14 @@ export const StreamPLayer = ({user, stream, isFollowing,}: StreamPLayerProps) =>
                         hostName={user.username}
                         hostIdentity={user.id}
                     />
+                    <Header 
+                        hostName={user.username}
+                        hostIdentity={user.id}
+                        viewerIdentity={identity}
+                        imageUrl={user.imageUrl}
+                        isFollowing={isFollowing}
+                        name={stream.name}
+                    />
                 </div>
                 <div className={cn(
                     "col-span-1 lg:col-span-1 xl:col-span-1 2xl:col-span-2",
@@ -75,7 +84,7 @@ export const StreamPlayerSkeleton = () => {
             <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 
                 2xl:col-span-4 lg:overflow-y-auto hidden-scrollbar pb-10">
                     <VideoSkeleton />
-                    {/* TODO: Header Skeleton */}
+                    <HeaderSkeleton />
             </div>
             <div className="col-span-1 2xl:col-span-2 bg-background">
                 <ChatSkeleton />
