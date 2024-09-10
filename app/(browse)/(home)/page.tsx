659ab2b56d964c40b647f31f7ee8b182f8460e48
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-09-10 13:31:42 +0530

    Enhance Home Page with publicly visible Feed

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -2,13 +2,16 @@
 // page.tsx / page.jsx are used to create Client Route   -->> like http://localhost:3000/folder where folder is the Client Route
 // route.tsx / route.jsx are used to create API Route  -->> {"foo": "bar"} API inside the 'localhost:3000//folder' pager
 
+import { Suspense } from "react";
+import { Results, ResultsSkeleton } from "./_components/results";
+
 export default function Page() {
   return (
     <>
-      <div className="flex flex-col mt-4 gap-y-8">
-        <h1>
-          HomePage
-        </h1>
+      <div className="h-full p-8 max-w-screen-2xl mx-auto">
+        <Suspense fallback={<ResultsSkeleton />}>
+          <Results />
+        </Suspense>
       </div>
     </>
   );
